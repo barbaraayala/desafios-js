1fc150d0fff7592b3d0d9d429021c7cee4553126
Barbara Ayala <barbaraayala22@gmail.com>
2022-09-03 01:19:07 -0300

    intento millll

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
+console.log("tablas de multiplicar")
+let num= parseInt(prompt("ingrese el numero de tabla que desea saber "))
+for(i = 0; i <= 10; i= i + 1){
+    
+    console.log(num, "X ", i, "= ", i*num)
+    
 
-for(let i=0; i<2; i++){
-    let nom= prompt("ingrese un nombre ");
-    alert("Hola "+ nom)
 }
 
